c6fed3902161d7ed7fdc5a4cad58b1d0a592b631
luquitas <47940674+Lucasorm3@users.noreply.github.com>
2020-08-07 09:32:40 -0300

    sd

--- content/JAVASCRIPT/COLORFLIPPER/app.js
+++ content/JAVASCRIPT/COLORFLIPPER/app.js
@@ -1,4 +1,4 @@
-const colors= ["green","red","rgba(133,122,200)","·f15025"];
+const colors= ["green","red","white","grey","black"];
 const btn = document.getElementById('btn');
 const color = document.querySelector(".color");
 
