deef8ee7a2bb4c4b5064efdb1c37f6017adf0bd4
luquitas <47940674+Lucasorm3@users.noreply.github.com>
2020-09-07 16:36:05 -0300

    Solf
    Imagenes corregidas. Falta grid. Y veremos.

--- solfloreado/assets/js/app.js
+++ solfloreado/assets/js/app.js
@@ -1,7 +1,7 @@
-let cart = document.querySelector(".cartNav");
-let c = 1;
-function openNav(event){
-    c = 2;
-}
-cart.onclick = openNav;
-console.log(c);
+const addCartbtns = document.querySelectorAll("addCart-ico");
+const cartItem = document.querySelector("cart-item");
+let count = 0;
+
+
+
+
